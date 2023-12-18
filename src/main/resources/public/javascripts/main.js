15bf99de1ec45826fb49bf2cae9e585ec6e5f86e
Marcos Pereira <marcos.silva@gmail.com>
2023-12-18 15:21:40 -0500

    ui: Add progress bar when running htmx requests

--- src/main/resources/public/javascripts/main.js
+++ src/main/resources/public/javascripts/main.js
@@ -11,14 +11,28 @@ function setupFootnotesAnimationEvent() {
                         noteElement.classList.add("blink-yellow");
                         // Remove the class after sometime so that the same footnote
                         // will blink when user clicks it a second time.
-                        setTimeout(() => {
-                            noteElement.classList.remove("blink-yellow");
-                        }, 3000)
+                        setTimeout(() => noteElement.classList.remove("blink-yellow"), 3000)
                     }
                 })
             });
     }
 }
 
+function delayHidingHtmxProgressBarWhenTooFast() {
+    const progressBar = document.getElementById("request-progress-bar");
+    if (progressBar) {
+        progressBar.classList.add("htmx-request");
+
+        // If the request completes too fast, this avoids the progress bar
+        // to just flicking.
+        setTimeout(() => progressBar.classList.remove('htmx-request'), 300);
+    }
+}
+
 window.addEventListener("load", setupFootnotesAnimationEvent);
+
+// https://htmx.org/events/#htmx:load
 document.documentElement.addEventListener("htmx:load", setupFootnotesAnimationEvent);
+
+// https://htmx.org/events/#htmx:afterRequest
+document.documentElement.addEventListener("htmx:afterRequest", delayHidingHtmxProgressBarWhenTooFast);
