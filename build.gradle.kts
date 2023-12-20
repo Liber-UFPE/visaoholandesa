aab7823f76732daa2f89d83de8ad6bd6f43b5049
Marcos Pereira <marcos.silva@gmail.com>
2023-12-20 10:40:50 -0500

    sonar: configure coverage exclusions

--- build.gradle.kts
+++ build.gradle.kts
@@ -137,6 +137,13 @@ sonar {
                         .filter(File::exists)
                 },
         )
+
+        // See docs here:
+        // https://docs.sonarsource.com/sonarqube/latest/project-administration/analysis-scope/#code-coverage-exclusion
+        property(
+            "sonar.coverage.exclusions",
+            listOf("**/*Generated*", "**/tasks/*.*"),
+        )
     }
 }
 
