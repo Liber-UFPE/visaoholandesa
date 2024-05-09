b29a8eb1c3ccdd9970a0356764e3dd3493b9b7e5
marcospereira <marcos.silva@gmail.com>
2024-05-09 20:27:55 +0000

    🔄 synced file(s) with Liber-UFPE/project-starter

--- src/test/kotlin/br/ufpe/liber/HttpClientExtensions.kt
+++ src/test/kotlin/br/ufpe/liber/HttpClientExtensions.kt
@@ -3,6 +3,9 @@ package br.ufpe.liber
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.BlockingHttpClient
 
+// DO NOT EDIT: this file is automatically synced from the template repository
+// in https://github.com/Liber-UFPE/project-starter.
+
 fun BlockingHttpClient.get(path: String): HttpResponse<String> = this.exchange(
     path,
     String::class.java,
